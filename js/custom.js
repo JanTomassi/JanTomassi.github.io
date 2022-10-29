20603f1aafd003bf96c4d1e22f781f907af8dfc5
Jan Tomassi <tomassijan@gmail.com>
2022-10-29 03:20:12 +0200

    Basic Template Skill section First page  small presentation

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+
+//
+$(document).ready(function () {
+	$('main').fullpage({
+		'navigation': true,
+		'scrollOverflow': true,
+		'touchSensitivity': 1,
+		'css3': true,
+	});
+});
+
+// wow
+$(function () {
+	//new WOW().init();
+	$(".rotate").textrotator({ speed: 3300 });
+})
