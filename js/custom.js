43a81df3a0b2b1b19eb510e3239a5a8799f9c836
Jan Tomassi <tomassijan@gmail.com>
2022-11-02 23:03:05 +0100

    Remove fullpage Delete unused script

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,6 @@
 
-//
-$(document).ready(function () {
-	$('main').fullpage({
-		'navigation': true,
-		'scrollOverflow': true,
-		'touchSensitivity': 1,
-		'css3': true,
-	});
-});
-
 // wow
 $(function () {
-	//new WOW().init();
+	new WOW().init();
 	$(".rotate").textrotator({ speed: 3300 });
 })
